cb522e621391cbad0dfd87234a2f161ee8efe52f
Julián Santiago Flórez Castañeda <jflorez33453@universidadean.edu.co>
2024-02-20 15:38:11 +0000

    LMS

---
+++ LibraryManagementSystem/src/Library.java
@@ -0,0 +1,18 @@
+public class Library {
+    
+    public class Book {
+        private String name;
+        private String author;
+        private int pages;
+        private int ISBN;
+        private String editorial;
+        private String collection;
+        private String category;
+        private String location;
+        private boolean avaliability;
+    }
+    public static void main(String[] args) {
+        
+    }
+
+}
