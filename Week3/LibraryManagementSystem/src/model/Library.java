a07880fc7346d857340e99c32b7d94390675b353
Julián Santiago Flórez Castañeda <jflorez33453@universidadean.edu.co>
2024-02-22 14:57:11 +0000

    LibraryManagementSystem

--- Week3/LibraryManagementSystem/src/model/Library.java
+++ Week3/LibraryManagementSystem/src/model/Library.java
@@ -6,6 +6,103 @@ import model.Member;
 import model.Librarian;
 
 public class Library {
+    
+    public class Book {
+        private String name;
+        private String author;
+        private int pages;
+        private int id;
+        private String isbn;
+        private String editorial;
+        private String collection;
+        private String category;
+        private String location;
+        private boolean avaliability;
+        private Section section;
+
+        public Book addBook(String name, String author, int pages, int id, String isbn ,String editorial, String collection, String category, String location, boolean avaliability, Section section) {
+            this.name = name;
+            this.author = author;
+            this.pages = pages;
+            this.id = id;
+            this.isbn = isbn;
+            this.editorial = editorial;
+            this.collection = collection;
+            this.category = category;
+            this.location = location;
+            this.avaliability = avaliability;
+            this.section = section;
+            section.addBook(this);
+            return this;
+        }  
+
+        public void lentBook() {
+            this.avaliability = false;
+        }
+
+        public void returnBook() {
+            this.avaliability = true;
+        }
+    }
+
+    public class Member{
+        private String name;
+        private int id;
+        private String email;
+        private ArrayList<Book> borrowedBooks = new ArrayList<Book>();
+
+        public Member addMember(String name, int id, String email) {
+            this.name = name;
+            this.id = id;
+            this.email = email;
+            return this;
+        }
+
+        public void borrowBook(Book book) {
+            this.borrowedBooks.add(book);
+            (book).lentBook();
+        }
+
+        public void returnBook(Book book) {
+            this.borrowedBooks.remove(book);
+            book.returnBook();
+        }
+    }
+
+    public class Librarian{
+        private String name;
+        private int id;
+        private Section section;
+        private String schedule;
+
+        public Librarian addLibrarian(String name, int id, Section section, String schedule) {
+            this.name = name;
+            this.id = id;
+            this.section = section;
+            this.schedule = schedule;
+            return this;
+        }
+    }
+
+    public class Section{
+        private String name;
+        private int id;
+        private String location;
+        private ArrayList<Book> books;
+
+        public Section addSection(String name, int id, String location, ArrayList<Book> books) {
+            this.name = name;
+            this.id = id;
+            this.location = location;
+            this.books = books;
+            return this;
+        }
+
+        public void addBook(Book book) {
+            this.books.add(book);
+        }
+    }
+
     public static void main(String[] args) {
        Library library = new Library();
 
