dc35dec4b857bcfe6c711423ef7e788dc9184675
Juan-Jose-Caicedo <159916915+Juan-Jose-Caicedo@users.noreply.github.com>
2024-02-29 14:16:24 +0000

    achu

--- Week3/OnlineShoppingPlatform/src/shoppingPackage/MethodOfPayment.java
+++ Week3/OnlineShoppingPlatform/src/shoppingPackage/MethodOfPayment.java
@@ -1,19 +1,18 @@
 package shoppingPackage;
 
 public class MethodOfPayment{
-    Customer customer;
-    String type;
-    String number;
-    String expirationDate;
-    String cvv;
+    private Customer customer;
+    private String type;
+    private String number;
+    private String expirationDate;
+    private String cvv;
 
-    public MethodOfPayment addMethodOfPayment(Customer customer, String type, String number, String expirationDate, String cvv) {
+    public MethodOfPayment(Customer customer, String type, String number, String expirationDate, String cvv) {
         this.customer = customer;
         this.type = type;
         this.number = number;
         this.expirationDate = expirationDate;
         this.cvv = cvv;
-        return this;
     }
 
     public void updateMethodOfPayment(String type, String number, String expirationDate, String cvv) {
@@ -21,5 +20,20 @@ public class MethodOfPayment{
         this.number = number;
         this.expirationDate = expirationDate;
         this.cvv = cvv;
+    }
+    public Customer getCustomer() {
+        return customer;
+    }
+    public String getType() {
+        return type;
+    }
+    public String getNumber() {
+        return number;
+    }
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+    public String getCvv() {
+        return cvv;
     }
 }
--- Week3/OnlineShoppingPlatform/src/shoppingPackage/Order.java
+++ Week3/OnlineShoppingPlatform/src/shoppingPackage/Order.java
@@ -1,12 +1,12 @@
 package shoppingPackage;
 
 public class Order{
-    int id;
-    Cart cart;
-    double total = 0.0;
-    MethodOfPayment methodOfPayment;
-    boolean processed = false;
-    boolean delivered = false;
+    private int id;
+    private Cart cart;
+    private double total = 0.0;
+    private MethodOfPayment methodOfPayment;
+    private boolean processed = false;
+    private boolean delivered = false;
 
     public Order addOrder(int id, Cart cart, MethodOfPayment methodOfPayment) {
         this.id = id;
