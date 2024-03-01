54c3016fa4d9d2782faf7cad3ead0a3d88b5fc20
Julián Santiago Flórez Castañeda <jflorez33453@universidadean.edu.co>
2024-02-29 21:07:45 -0500

    SchoolManagementSystem

--- Week3/SchoolManagementSystem/src/schoolPackage/School.java
+++ Week3/SchoolManagementSystem/src/schoolPackage/School.java
@@ -7,8 +7,11 @@ public class School {
         Student student1 = new Student("Jane Doe", 1, classroom1);
         Student student2 = new Student("John Smith", 2, classroom1);
         Schedule schedule1 = new Schedule("Monday", "9:00", math, classroom1);
+        System.out.println(schedule1);
         Grade grade1 = new Grade(math, 90, student1);
         Grade grade2 = new Grade(math, 85, student2);
+        System.out.println(grade1);
+        System.out.println(grade2);
         System.out.println(classroom1);
 
     }
