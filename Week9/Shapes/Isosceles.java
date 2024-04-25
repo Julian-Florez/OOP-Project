c5b9786b1aa4993c39d89e67d85a0018f5383528
Julián Santiago Flórez Castañeda <jflorez33453@universidadean.edu.co>
2024-04-25 14:22:52 +0000

    allala

--- Week9/Shapes/Isosceles.java
+++ Week9/Shapes/Isosceles.java
@@ -1,6 +1,16 @@
 public class Isosceles extends Triangle{
-    super(side1, side1, side2, angle1, angle1, angle2);
     private double base;
+
+    public Isosceles(double side1, double side2, double base, double angle1, double angle2, double angle3) {
+        super(side1, side2, side1, angle1, angle2, angle3);
+        this.base = base;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 0.5 * base * Math.sqrt(side1 * side1 - 0.25 * base * base);
+    }
+    
     
 
 
--- Week9/Shapes/Scalene.java
+++ Week9/Shapes/Scalene.java
@@ -2,4 +2,10 @@ public class Scalene extends Triangle{
     public Scalene(double side1, double side2, double side3, double angle1, double angle2, double angle3) {
         super(side1, side2, side3, angle1, angle2, angle3);
     }
+
+    @Override
+    public double calculateArea() {
+        double s = (side1 + side2 + side3) / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
 }
